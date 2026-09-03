cebb3f1ac38f64e57336cd3ec4fd4c072ad892f2
1wenwen1 <wen19970221@gmail.com>
2026-09-04 07:53:34 +0800

    Remove obsolete init2 deployment script

--- deployment/sourcefile/init2.sh
+++
@@ -1,8 +0,0 @@
-rm -rf achillies
-cd ~
-git clone https://github.com/VSEJGFB/achillies.git 
-cd achillies
-git submodule init
-git submodule update
-(cd salticidae; cmake . -DCMAKE_INSTALL_PREFIX=.; make; make install)
-
